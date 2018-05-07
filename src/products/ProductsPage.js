30308fdc906c3e58ebcfe462b40fb491fda896f9
maxwell-oroark <m.d.oroark@gmail.com>
2018-05-07 13:09:23 -0600

    try different path

--- src/products/ProductsPage.js
+++ src/products/ProductsPage.js
@@ -4,9 +4,9 @@ import ProductImageAndMetadata from '../components/ProductImageAndMetadata';
 import ImageBorderBoxSectionOne from '../components/ImageBorderBoxSectionOne';
 import ImageBorderBoxSectionTwo from '../components/ImageBorderBoxSectionTwo';
 
-import NutritionReceiptImage from '/assets/POTMJalapenos.png';
-import BorderImageSectionOne from '/assets/carrots-food-fresh.jpg';
-import BorderImageSectionTwo from '/assets/cauliflower-food-fresh.jpg';
+import NutritionReceiptImage from '../../static/assets/POTMJalapenos.png';
+import BorderImageSectionOne from '../../static/assets/carrots-food-fresh.jpg';
+import BorderImageSectionTwo from '../../static/assets/cauliflower-food-fresh.jpg';
 
 export default class ProductsPage extends Component {
   render() {
